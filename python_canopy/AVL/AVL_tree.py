89cdcd3c62e94dd4099490b3f36c221ef98a3f64
Carlos Alexandre <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-04-07 21:43:02 -0300

    Remove unused imports and functions and format the code

--- python_canopy/AVL/AVL_tree.py
+++ python_canopy/AVL/AVL_tree.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
-from queue import Queue
 import random
-from typing import Any, Optional
+from typing import Optional
 
 
 @dataclass
 class Node:
     value: int  # key or value, is what we are checking for balance, can be int or Any
+    # data: Any   # data, what we are storing, can be int or Any
     left: Optional["Node"] = None
     right: Optional["Node"] = None
     parent: Optional["Node"] = None
@@ -529,105 +529,3 @@ class AVLTree:
         self._postorder(node.left)
         self._postorder(node.right)
         print(node.value, end=' ')
-
-
-import graphviz
-
-
-class AVLTreeGraph(AVLTree):
-    """ AVL Tree Graph Class """
-
-    def __init__(self):
-        """ Initialize the AVL Tree Graph """
-        super().__init__()
-
-    def _graphviz(self, node: Optional[Node], graph: graphviz.Digraph) -> None:
-        """ Graphviz function to create the graph
-        
-        Args:
-            node: the node to add to the graph
-            graph: the graph to add the node to
-        """
-        # if the node is None, return
-        if node is None:
-            return
-
-        # otherwise add the node to the graph
-        graph.node(str(node.value), str(node.value))
-
-        # if the node has a left child, add the edge to the graph
-        if node.left is not None:
-            graph.edge(str(node.value), str(node.left.value))
-
-        # if the node has a right child, add the edge to the graph
-        if node.right is not None:
-            graph.edge(str(node.value), str(node.right.value))
-
-        # recursively call the function on the left and right subtrees
-        self._graphviz(node.left, graph)
-        self._graphviz(node.right, graph)
-
-    def graphviz(self) -> graphviz.Digraph:
-        """ Graphviz function to create the graph
-        
-        Returns:
-            graphviz.Digraph: the graph
-        """
-        # create the graph
-        graph = graphviz.Digraph()
-
-        # call the graphviz function
-        self._graphviz(self.root, graph)
-
-        # return the graph
-        return graph
-
-
-# test the AVL tree
-# create an AVL tree
-tree = AVLTree()
-
-# list
-lst = [
-    59, 16, 9, 8, 0, 6, 15, 41, 27, 23, 19, 17, 21, 40, 39, 32, 38, 53, 49, 47,
-    46, 43, 48, 50, 55, 95, 72, 69, 64, 61, 65, 71, 89, 76, 73, 74, 85, 93, 96
-]
-
-# fill the tree with the list
-for i in lst:
-    tree.insert(i)
-
-# fill the tree with random values
-#tree.fill_tree_random(50, 100)
-
-# fill the tree with ordered values
-#tree.fill_tree_ordered(50)
-
-# print the tree in order
-print('Inorder:')
-tree.print_tree_inorder()
-
-# print the tree in preorder
-print('Preorder:')
-tree.print_tree_preorder()
-
-# print the tree in postorder
-print('Postorder:')
-tree.print_tree_postorder()
-
-# print the tree in level order
-print('Level order:')
-tree.print_level_order()
-
-# create avl tree graph
-avl_tree_graph = AVLTreeGraph()
-
-# fill the tree with the list
-for i in lst:
-    avl_tree_graph.insert(i)
-
-# render the graph
-graph = avl_tree_graph.graphviz()
-
-# render the graph
-graph.render(filename='avl_tree_graph', format='png')
