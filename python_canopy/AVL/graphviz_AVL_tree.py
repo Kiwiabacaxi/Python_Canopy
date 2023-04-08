ed761bb5f9baffd1fff733023fc7eff56901ee84
Carlos Alexandre <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-04-07 21:44:53 -0300

    Added the AVLTreeGraph class

--- python_canopy/AVL/graphviz_AVL_tree.py
+++ python_canopy/AVL/graphviz_AVL_tree.py
@@ -3,53 +3,78 @@ from AVL_tree import AVLTree, Node
 import graphviz
 
 
-class AVLTreeGraph(AVLTree):
+class AVLTreeGraph(AVLTree):  #
     """ AVL Tree Graph Class """
 
     def __init__(self):
         """ Initialize the AVL Tree Graph """
         super().__init__()
 
-    def _graphviz(self, node: Optional[Node], graph: graphviz.Digraph) -> None:
-        """ Graphviz function to create the graph
-        
+    def visualize_avl(self, tree: Optional[Node], graph: graphviz.Digraph):
+        """ Visualize an AVL tree using graphviz.
+
         Args:
-            node: the node to add to the graph
-            graph: the graph to add the node to
+            tree (AVLTree): The AVL tree to visualize.
+            graph (Digraph): The graphviz graph to add the tree to.
         """
-        # if the node is None, return
-        if node is None:
-            return
 
-        # otherwise add the node to the graph
-        graph.node(str(node.value), str(node.value))
+        # customize the graph
+        graph.engine = "dot"
 
-        # if the node has a left child, add the edge to the graph
-        if node.left is not None:
-            graph.edge(str(node.value), str(node.left.value))
+        graph.attr("node",
+                   shape="circle",
+                   fontcolor="black",
+                   fontname="Arial",
+                   fontsize="12",
+                   width="0.5",
+                   height="0.5")
 
-        # if the node has a right child, add the edge to the graph
-        if node.right is not None:
-            graph.edge(str(node.value), str(node.right.value))
+        # if tree is not empty
+        if tree is not None:
+            # if left child is not empty
+            if tree.left is not None:
+                # add edge from parent to left child
+                graph.edge(str(tree.value), str(tree.left.value))
+            # if right child is not empty
+            if tree.right is not None:
+                # add edge from parent to right child
+                graph.edge(str(tree.value), str(tree.right.value))
+            # recursively call visualize_avl on left child
+            self.visualize_avl(tree.left, graph)
+            # recursively call visualize_avl on right child
+            self.visualize_avl(tree.right, graph)
 
-        # recursively call the function on the left and right subtrees
-        self._graphviz(node.left, graph)
-        self._graphviz(node.right, graph)
+    def find_node(self, tree: Optional[Node], graph: graphviz.Digraph,
+                  value: int):
+        """ Find a node in an AVL tree and change its color to green.
 
-    def graphviz(self) -> graphviz.Digraph:
-        """ Graphviz function to create the graph
-        
-        Returns:
-            graphviz.Digraph: the graph
+        Args:
+            tree (AVLTree): The AVL tree to find the node in.
+            graph (Digraph): The graphviz graph to add the tree to.
+            value (int): The value of the node to find.
         """
-        # create the graph
-        graph = graphviz.Digraph()
 
-        # call the graphviz function
-        self._graphviz(self.root, graph)
+        # customize the graph
+        graph.engine = "dot"
 
-        # return the graph
-        return graph
+        graph.attr("node",
+                   shape="circle",
+                   fontcolor="black",
+                   fontname="Arial",
+                   fontsize="12",
+                   width="0.5",
+                   height="0.5")
+
+        # if tree is not empty
+        if tree is not None:
+            # if the value is found
+            if tree.value == value:
+                # change the color of the node to green
+                graph.node(str(tree.value), color="green", style="filled")
+            # recursively call find_node on left child
+            self.find_node(tree.left, graph, value)
+            # recursively call find_node on right child
+            self.find_node(tree.right, graph, value)
 
 
 # create a tree
@@ -58,15 +83,24 @@ avl = AVLTreeGraph()
 # list
 lst = [
     59, 16, 9, 8, 0, 6, 15, 41, 27, 23, 19, 17, 21, 40, 39, 32, 38, 53, 49, 47,
-    46, 43, 48, 50, 55, 95, 72, 69, 64, 61, 65, 71, 89, 76, 73, 74, 85, 93, 96
+    46, 43, 48, 50, 55, 95, 72, 69, 64, 61, 65, 71, 89, 76, 73, 74, 85, 93, 96,
+    100
 ]
 
 # insert nodes
 for i in lst:
     avl.insert(i)
 
-# graph the tree
-avl.graphviz()
+# create a graph
+graph = graphviz.Digraph()
+
+# visualize the tree
+avl.visualize_avl(avl.root, graph)
+
+# find a node
+avl.find_node(avl.root, graph, 59)
 
 # save the graph
-avl.graphviz().render(directory='python_canopy/AVL/avl_graph',filename='avl_tree_graph', format='png')
+graph.render(directory='python_canopy/AVL/avl_graph',
+             filename='avl_tree_graph',
+             format='png')
